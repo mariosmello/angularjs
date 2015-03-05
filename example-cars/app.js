9d488d3f90564255baf3026c5d0e5de76131493e
Mario Mello <mario@dindigital.com>
2015-03-05 07:30:13 -0300

    Aplicando filtro usando dropdown

--- example-cars/app.js
+++ example-cars/app.js
@@ -2,6 +2,7 @@ angular.module('carsApp', [])
     .controller('ModelsCtrl', ['$http', function($http) {
         var self = this;
         self.models = [];
+        self.model = [];
         self.stock = [];
         self.customer = [];
 
@@ -10,18 +11,33 @@ angular.module('carsApp', [])
             self.models = response.data.message;
             self.stock = [];
             self.customer = [];
+            self.model = [];
         }, function (errResponse) {
             console.error('erro');
         });
 
-        // Busca estoque de um modelo
-        self.findStock = function() {
-            $http.get('stock.json').then(function(response) {
+        // Busca detalhes e estoque de um modelo
+        self.findStock = function(model) {
+
+            self.stock = [];
+            self.customer = [];
+            self.model = [];
+
+            var id_model = model.id_model;
+
+            $http.get('model.json?id_model='+id_model).then(function(response) {
+                self.model = response.data.message;
+            }, function (errResponse) {
+                console.error('erro');
+            });
+
+            $http.get('stock.json?id_model='+id_model).then(function(response) {
                 self.stock = response.data.message;
                 self.customer = [];
             }, function (errResponse) {
                 console.error('erro');
             });
+
         };
 
         // Busca vendedor do estoque
