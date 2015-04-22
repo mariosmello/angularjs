85d7b59354d5bd9a4862b1213924c2ceae23abd2
Mário Mello <mario@dindigital.com>
2015-04-21 22:36:12 -0300

    Exemplo de uso de directive com $attrs

--- example-async/app.js
+++ example-async/app.js
@@ -1,14 +1,35 @@
-var app = angular.module('newsApp', []);
+var app = angular.module('asyncApp', []);
 
 app.controller('LastedController', ['LastedService', function(LastedService) {
+
     var self = this;
     self.news = [];
 
     LastedService.getLasted().then(function(response) {
         self.news = response.data.news;
     });
+
 }]).directive('newsLasted', function() {
+
     return {
         templateUrl: 'templates/lasted.html'
     };
+
+});
+
+app.controller('RelatedController', ['$attrs','RelatedService', function($attrs, RelatedService) {
+
+    var self = this;
+    self.news = [];
+
+    RelatedService.getRelated($attrs.tbl,$attrs.id).then(function(response) {
+        self.news = response.data.news;
+    });
+
+}]).directive('newsRelated', function() {
+
+    return {
+        templateUrl: 'templates/related.html'
+    };
+
 });
--- example-async/services.js
+++ example-async/services.js
@@ -1,9 +1,21 @@
-angular.module('newsApp')
-  .factory('LastedService', ['$http',
-    function($http) {
-      return {
-        getLasted: function() {
-          return $http.get('data/lasted.json');
-        }
-      }
-  }]);
+var service = angular.module('asyncApp');
+
+service.factory('LastedService', ['$http',
+function($http) {
+  return {
+    getLasted: function() {
+      return $http.get('data/lasted.json');
+    }
+  }
+}]);
+
+service.factory('RelatedService', ['$http',
+function($http) {
+  return {
+    getRelated: function(tbl,id) {
+      var url = 'data/related.json?tbl=' + tbl + '&id=' + id;
+      console.log(url);
+      return $http.get('data/related.json');
+    }
+  }
+}]);
